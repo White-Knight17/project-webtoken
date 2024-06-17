8bc6b74a0a02692f7de15cbcdb8b2e1c3c202971
German Giorgio <germangiorgio1998@gmail.com>
2024-06-17 15:13:26 -0300

    Fix Auth not found

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import authRouter from "../src/router/authRoutes";
+import authRouter from "./router/authRoutes";
 import userRouter from "./router/userRouter";
 
 dotenv.config();
--- src/router/authRoutes.ts
+++ src/router/authRoutes.ts
@@ -5,8 +5,8 @@ import { login, register } from "../controller/authController";
 
 const router = express.Router()
 
-router.post('/register',register);
+router.post('/register', register);
 router.post('/login', login);
 
 
-export default router
+export default router;
